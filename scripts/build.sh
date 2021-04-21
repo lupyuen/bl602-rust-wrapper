0dc624f86dea61cf36143664b988cb55b7e63d17
lupyuen <luppy@appkaki.com>
2021-04-22 07:29:59 +0800

    Expand macros

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,12 +4,22 @@ set -e  #  Exit when any command fails.
 set -x  #  Echo all commands.
 export RUST_BACKTRACE=1  #  Show Rust errors.
 
+rust_build_options=" \
+    --target riscv32imacf-unknown-none-elf.json \
+    -Z build-std=core \
+"
+
 pushd bl602-macros
 cargo build
 popd
 
 pushd bl602-sdk
+cargo rustc \
+    $rust_build_options \
+    -- -Z unstable-options \
+    --pretty expanded \
+    > ../logs/sdk-expanded.rs
+
 cargo build \
-    --target riscv32imacf-unknown-none-elf.json \
-    -Z build-std=core
+    $rust_build_options
 popd
