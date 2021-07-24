c49596e057901eb1ba75e6fcbc6e94b8cbb77635
lupyuen <luppy@appkaki.com>
2021-07-24 13:38:28 +0800

    Clean up

--- bl602-sdk/src/lib.rs
+++ bl602-sdk/src/lib.rs
@@ -313,7 +313,7 @@ pub type Ptr = *mut ::cty::c_void;
 /// For Testing: This function will be called by the BL602 command-line interface
 #[no_mangle]              //  Don't mangle the function name
 extern "C" fn test_rust(  //  Declare `extern "C"` because it will be called by BL602 firmware
-    _result: *mut u8,        //  Result to be returned to command-line interface
+    _result: *mut u8,        //  Result to be returned to command-line interface (char *)
     _len:  i32,              //  Length of command line (int)
     _argc: i32,              //  Number of command line args (int)
     _argv: *const *const u8  //  Array of command line args (char **)
